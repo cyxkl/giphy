3738d209809303b236170b990820a6846e94f3bc
Glitch (skillful-rumbling-stew) <none>
2021-04-30 00:46:10 +0000

    🍭📠 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -2,10 +2,25 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
-$(".search-button").click(function(){
+$(".search-button").click(function() {
   
-
+  let input = $(".search-term").val();
+  let api_url ="https://api.giphy.com/v1/gifs/search?q=" + input + "&rating=pg&api_key=dc6zaTOxFJmzC";
+  console.log(input);
+  fetch(api_url)
   
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(gifData) {
+      let ranImg = Math.floor(Math.random() * gifData.data.length); 
+      $(".main").html(`<img src="${gifData.data[ranImg].images.original.url}" alt="">`);
+    });
 });
 
+
+$(".search-term").keyup(function(event) {
+  if(event.keyCode === 13) {
+    $(".search-button").click();
+  }
+});
